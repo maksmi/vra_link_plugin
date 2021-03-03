0441e27d40fc5be4cf56acc5924cf2113438b2e7
Maksim.Ring <Maksim.Ring@billing.ru>
2021-03-04 02:30:59 +0300

    version 0.3 , bookmarks

--- popup.js
+++ popup.js
@@ -7,7 +7,6 @@ HTMLDocument.prototype.ready = new Promise(function (resolve) {
         });
 });
 
-
 document.ready.then(function () {
     create_list()
 });
@@ -24,13 +23,38 @@ function add_item(url, name, port, path = '') {
     document.querySelector('.row-content').appendChild(tag)
 }
 
-function check_url() {
-    chrome.tabs.getSelected(null, function (tab) {
-        if (tab.url.startsWith('http://srv8-')) {
-            return true
+function add_bookmarks(url) {
+    console.log("you click button");
+    chrome.bookmarks.search(url.hostname, function (results) {
+        if (results.length === 0) {
+            console.log("Adding bookmark");
+            add_bookmarks_template(url.hostname);
         }
-        return false
     });
+}
+
+function add_bookmarks_template(vra_name) {
+    chrome.bookmarks.create({
+            'parentId': "1",
+            'title': vra_name
+        },
+        function (newFolder) {
+            console.log("added folder: " + newFolder.title);
+            console.log(newFolder.id);
+            chrome.storage.sync.get({
+                linksListAsObj: []
+            }, function (items) {
+                items.linksListAsObj.forEach(function (entry) {
+                    add_item(url, entry.name, entry.port, entry.path)
+                    chrome.bookmarks.create({
+                        'parentId': newFolder.id,
+                        'title': entry.name,
+                        'url': "http://" + vra_name + ":" + entry.port + entry.path
+                    });
+                });
+            });
+        });
+
 }
 
 function create_list() {
@@ -39,13 +63,20 @@ function create_list() {
             var url = new URL(tab.url)
             chrome.storage.sync.get({
                 linksListAsObj: []
-            }, function(items) {
-                items.linksListAsObj.forEach(function(entry) {
+            }, function (items) {
+                items.linksListAsObj.forEach(function (entry) {
                     add_item(url, entry.name, entry.port, entry.path)
                 });
             });
+            let button = document.createElement("button");
+            button.innerText = "ADD TO BOOKMARKS"
+            button.onclick = function () {
+                add_bookmarks(url);
+            };
+            document.querySelector('.row-content').appendChild(button)
         } else {
             document.querySelector('.row-content').innerHTML = "NOT VRA";
         }
     });
 }
+
